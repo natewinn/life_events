1c12175dabb97337bbe11aded4387b4b89f3ba29
Nate Winn <nathan.winn@gmail.com>
2014-04-17 08:35:24 -0400

    Events table functional

--- app.rb
+++ app.rb
@@ -3,19 +3,37 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:///events_data.db"
 
-## LIST ##
+# -------------------------------------->
+
+## List All Items // School ##
 get "/schools" do
 	@schools_attended = School.all
 	erb :"/schools/index"
 end
 
-## VIEW ##
+## List All Items // Life Event ##
+get "/life_events" do
+	@events_experienced = Event.all
+	erb :"/life_events/index"
+end
+
+# -------------------------------------->
+
+## View Individual Records // School ##
 get "/schools/:id" do 
 	@attended = School.find(params[:id])
 	erb :"schools/show"
 end
 
-## ADD ##
+## View Individual Records // Life Event ##
+get "/life_events/:id" do 
+	@experienced = Event.find(params[:id])
+	erb :"life_events/show"
+end
+
+# -------------------------------------->
+
+## Add New Records // School ##
 post "/schools" do
 	puts params[:school]
 	school = School.new(params[:school])
@@ -24,21 +42,51 @@ post "/schools" do
 	end
 end	
 
-## DELETE ##
+## Add New Records // Life Events ##
+post "/life_events" do
+	puts params[:event]
+	event = Event.new(params[:event])
+	if event.save
+		redirect "/life_events"
+	end
+end	
+
+# -------------------------------------->
+
+## Delete Existing Record // School ##
 delete '/schools/:id' do
 	@attended = School.find(params[:id])
 	@attended.destroy
 	redirect '/schools'
 end
 
+## Delete Existing Record // Life Events ##
+delete '/life_events/:id' do
+	@experienced = Event.find(params[:id])
+	@experienced.destroy
+	redirect '/life_events'
+end
 
-## UPDATE ##
+# -------------------------------------->
+
+## Update Exisiting Record // School ##
 put "/schools/:id" do
 	@attended = School.find(params[:id])
 	@attended.update_attributes(params[:school])
 		redirect "/schools/#{@attended.id}"
-	end
 end
 
+## Update Exisiting Record // Life Events ##
+put "/life_events/:id" do
+	@experienced = Event.find(params[:id])
+	@experienced.update_attributes(params[:event])
+		redirect "/life_events/#{@experienced.id}"
+end
+
+# -------------------------------------->
+
 class School < ActiveRecord::Base
+end
+
+class Event < ActiveRecord::Base
 end
